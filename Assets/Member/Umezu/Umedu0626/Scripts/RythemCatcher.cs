3232c86fa93be1bc1d7c50075df50bc04626a39c
natureumedu <julyumedu@gmail.com>
2015-06-26 18:16:31 +0900

    音２０以上かつJumpならplayer左から失禁パーティクル
    Signed-off-by: natureumedu <julyumedu@gmail.com>

---
+++ Assets/Member/Umezu/Umedu0626/Scripts/RythemCatcher.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class RythemCatcher : MonoBehaviour {
+	public int Attackpower;
+	public bool Attack = true;
+	// Use this for initialization
+	void Start () {
+		p=Attackpower * 3;
+		obj =GameObject.Find("particle_burst");
+		obj.GetComponent<ParticleSystem> ().Stop();
+	}
+	GameObject obj;
+	private float[] waveData_ = new float[1024];
+	int p;
+	void Update()
+	{
+		//obj.GetComponent<ParticleSystem> ().Stop();
+
+		}
+
+		public void OnClick() {
+
+		AudioListener.GetOutputData (waveData_, 1);
+		var volume = waveData_.Select (x => x * x).Sum () / waveData_.Length * 100;//音量取得
+			Debug.Log("Button click!");
+//			// 非表示にする
+//			gameObject.SetActive(false);
+//			// Buttonを表示する
+//			MyCanvas.SetActive("Button", true);
+		print ("volume "+volume+"Attack " + Attack);
+	if (volume > 20 && Attack) {//音量２０以上Attack=trueなら
+			 
+			Attackpower = p;//攻撃３倍
+			//GameObject obj =	GameObject.Find(typeof("Particle System"));
+
+			//GameObject obj =transform.FindChild ("Particle System").gameObject
+		obj.GetComponent<ParticleSystem> ().Play ();//succes particl
+		print ("Attackpower " + Attackpower);
+
+		}
+}
+}
---
+++ Assets/Member/Umezu/Umedu0626/Scripts/WaveOutputterText.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+
+public class WaveOutputterText : MonoBehaviour 
+{
+	
+	private float[] waveData_ = new float[1024];
+	
+	void Update()
+	{
+		AudioListener.GetOutputData(waveData_, 1);
+		var volume = waveData_.Select(x => x*x).Sum() / waveData_.Length*100;
+	//	transform.localScale = Vector3.one * volume * 20;
+
+		//メインコード部
+		Text msg = GameObject.Find("Canvas/Text").GetComponent<Text>();
+		msg.color = Color.red;
+		msg.text = volume.ToString();
+	}
+}
