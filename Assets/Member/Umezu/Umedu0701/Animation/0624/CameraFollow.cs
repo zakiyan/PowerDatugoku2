755e88cd20a48e7fa25f33ebe547ad5cdf1f3965
natureumedu <julyumedu@gmail.com>
2015-07-01 20:58:46 +0900

    sound bar追加
    Signed-off-by: natureumedu <julyumedu@gmail.com>

---
+++ Assets/Member/Umezu/Umedu0701/Animation/0624/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public float smoothing = 5f;
+	Vector3 offset;
+	Vector3 pos ;
+
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = target.position + offset;
+		pos = new Vector3 (targetCamPos.x-30, 30+100, targetCamPos.z+100);
+		//transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+		transform.position = Vector3.Lerp (transform.position, pos, smoothing * Time.deltaTime);
+	}
+}
---
+++ Assets/Member/Umezu/Umedu0701/Scripts/SongScanbar.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+public class SongScanbar : MonoBehaviour {
+	TrailRenderer trail;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		//delay play
+		audioSource.clip = audioClip;
+		audioSource.time = 10f;
+		audioSource.Play();
+	}
+	private float[] waveData_ = new float[1024];
+	public AudioClip audioClip;
+	public AudioSource audioSource;
+	// Update is called once per frame
+	void Update () {
+		AudioListener.GetOutputData(waveData_, 1);
+		var volume = waveData_.Select(x => x*x).Sum() / waveData_.Length*100;
+		//	transform.localScale = Vector3.one * volume * 20;
+		//add bar
+		//audioSource.clip = GetComponent.<AudioSource>();
+		//AudioSource.PlayDelayed(5);
+
+
+		//volume limitter
+		if (volume < 20) {
+			volume =20;
+		}else{
+			//GetComponent<TrailRenderer>().enabled=false;
+		}
+
+		transform.position = new Vector3( Time.time * speed+100,volume+100,100);
+		 
+
+	}
+}
