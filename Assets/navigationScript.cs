22e69601322da33843d7d9cc99712c4332e92a5f
Takakaito <takayanagitaka@gmail.com>
2015-07-03 19:24:38 +0900

    Plane追加

---
+++ Assets/navigationScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class navigationScript : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<NavMeshAgent> ().SetDestination (target.transform.position);
+	
+	}
+}
