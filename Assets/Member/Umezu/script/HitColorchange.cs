f949092852f3d295a532b5929f6e633bbf7fcfff
natureumedu <julyumedu@gmail.com>
2015-07-07 20:01:48 +0900

    ミキサー追加
    Signed-off-by: natureumedu <julyumedu@gmail.com>

---
+++ Assets/Member/Umezu/script/HitColorchange.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HitColorchange : Button {
+	
+	public bool IsOn { get; private set;}
+	
+	public override void OnPointerDown (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		GetComponent<Image> ().color = Color.red;
+		base.OnPointerDown (eventData);
+		IsOn = true;
+	}
+	
+	public override void OnPointerUp (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerUp (eventData);
+		IsOn = false;
+	}
+	
+	public override void OnPointerExit (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerExit (eventData);
+		IsOn = false;
+	}
+	
+	// これはイラナイ
+	void Update()
+	{
+		Debug.Log("押されている？ " + IsOn);
+	}
+}
---
+++ Assets/Member/Umezu/script/RepeatButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RepeatButton : MonoBehaviour 
+{
+	bool push = false; // ボタンが押されているか？
+	
+	public void StartPush()
+	{
+		push = true;
+	}
+	
+	public void StopPush()
+	{
+
+		push = false;
+	}
+	
+	void Update()
+	{
+		if (push) {
+			//ここにやらせたい処理を書きます
+			Debug.Log ("押されてます！");
+			GameObject.Find ("TestButton").GetComponent<Image> ().color = Color.red;
+		} 
+			GameObject.Find ("TestButton").GetComponent<Image> ().color = Color.white;
+
+	}
+}
