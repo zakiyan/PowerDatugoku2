0384db461e689a9bd4a3348bda064004173b4a37
kane49 <kanedanachi7@gmail.com>
2015-07-09 19:23:18 +0900

    Stage統合コミット

---
+++ Assets/Member/Kaneda/Scripts/GoMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoMenu : MonoBehaviour {
+
+	public string loadlevelName;
+
+	public void LoadScene(){
+		Application.LoadLevel(loadlevelName);
+	}
+}
