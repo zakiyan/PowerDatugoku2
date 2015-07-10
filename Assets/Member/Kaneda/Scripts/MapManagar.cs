6781145c2fd3635460c3d76f3f934f65a47542e3
kane49 <kanedanachi7@gmail.com>
2015-07-10 18:05:42 +0900

    MapManagar.csを変更

--- Assets/Member/Kaneda/Scripts/MapManagar.cs
+++ Assets/Member/Kaneda/Scripts/MapManagar.cs
@@ -5,6 +5,10 @@ public class MapManagar : MonoBehaviour {
 
 	private Camera subcamera;
 	private Camera subcameraPoi;
+	private bool ismapScale = false;
+	public GameObject MapBtn;
+	private 
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,19 +16,24 @@ public class MapManagar : MonoBehaviour {
 //		subcameraPoi = subcamera;
 //		subcameraPoi.rect = subcamera.rect;
 		Debug.Log("SubPosiGet!!");
+
 	}
 	
 	// Update is called once per frame
 	public void OnClick()
 	{
-		if (Time.timeScale == 1f) {
-			Time.timeScale = 0f;
-			subcamera.rect = new Rect (0.25F, 0.1F, 0.48F, 0.8f);
-		} else {
 
+		if (ismapScale ==false) {
+//			Time.timeScale = 0f;
+//			subcamera.rect = new Rect (0.25F, 0.1F, 0.48F, 0.8f);
+			subcamera.rect = new Rect (0.66f, 0.4F, 0.38F, 0.61f);
+			MapBtn.transform.localScale = new Vector3(4, 5, 1);
+			ismapScale = true;
+		} else {
 			subcamera.rect = new Rect (0.87f, 0.8f, 0.2f, 0.2f);
 			Debug.Log("もとにもどれ！!!");
-			Time.timeScale = 1f;
+			MapBtn.transform.localScale = new Vector3(1, 1, 1);
+			ismapScale = false;
 		}
 	}
 }
