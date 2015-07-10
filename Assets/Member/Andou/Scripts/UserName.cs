c5f91431c4a18140683b0bd7abb5e073df64f530
undoxxx <undo.pc@gmail.com>
2015-07-10 19:41:17 +0900

    Assets/MainMenu0710_ando.unity.meta　に　ユーザー名の入力を実装。　Stageへの引継ぎはまだなので誰か…

---
+++ Assets/Member/Andou/Scripts/UserName.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UserName : MonoBehaviour {
+
+	public string userName;
+	public InputField inputField;
+	public Text text;
+	
+	public void SaveUserName () {
+		userName = inputField.text;
+		text.text = userName;
+		// inputField.text = "";
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
