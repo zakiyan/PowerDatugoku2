b196e7356aa5cf38da9831b36f3a8960acec5c1b
kane49 <kanedanachi7@gmail.com>
2015-07-13 20:30:17 +0900

    タイムが記録されるようにしました。

---
+++ Assets/FastTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FastTime : MonoBehaviour {
+
+	public Text FastTimeText;
+	private float Timer;
+	// Use this for initialization
+	void Start () 
+	{
+		Timer = PlayerPrefs.GetFloat ("FastTimeKey", 5000.0f);
+
+		FastTimeText.text = ((int)(Timer / 60)).ToString ("00") + ":" 
+			+ (((int)Timer) % 60).ToString ("00") + ":"
+				+ (Mathf.Floor (Timer * 1000) - (((int)Timer) * 1000)).ToString ("0,0,0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Member/Kaneda/Scripts/TimeManagar.cs
+++ Assets/Member/Kaneda/Scripts/TimeManagar.cs
@@ -8,13 +8,16 @@ public class TimeManagar : MonoBehaviour {
 	//スコア表示するテキストをインスペクター上からアタッチ
 	public Text TimeText;
 	private float Timer = 0f;
+	private float fastTime;
 	private System.TimeSpan deltaTime;
 	private System.DateTime deltaTime2;
 
 	private string datetimeString;
 	
 	void Start () {
-		startTime = System.DateTime.Now;
+
+		fastTime = PlayerPrefs.GetFloat ("FastTimeKey", 1000.0f);
+		//startTime = System.DateTime.Now;
 
 		// 時刻の保存
 		//System.DateTime now = System.DateTime.Now;
@@ -43,18 +46,22 @@ public class TimeManagar : MonoBehaviour {
 //			System.DateTime goalNow = now;
 //
 //
-//			//if()
-//
 //			PlayerPrefs.SetString ("key", deltaTime2.ToBinary().ToString() );//deltaTime
 //		}
 
-		Timer += Time.deltaTime;
-
-		TimeText.text =  ((int)(Timer/60)).ToString("00")
-			+":" 
-			+ (((int)Timer)%60).ToString("00") 
-			+ ":"
-				+(Mathf.Floor(Timer*1000)-(((int)Timer)*1000)).ToString("0,0,0");
-				  ;
+		if (Goal.isGoal == false) {
+			Timer += Time.deltaTime;
+			
+			TimeText.text = ((int)(Timer / 60)).ToString ("00") + ":" 
+				+ (((int)Timer) % 60).ToString ("00") + ":"
+				+ (Mathf.Floor (Timer * 1000) - (((int)Timer) * 1000)).ToString ("0,0,0");
+		} else 
+		{
+			if(fastTime>Timer)
+			{
+				PlayerPrefs.SetFloat("FastTimeKey",Timer);
+				PlayerPrefs.Save();
+			}
+		}
 	}
 }
