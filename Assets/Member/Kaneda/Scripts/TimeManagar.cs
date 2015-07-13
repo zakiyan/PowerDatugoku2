28265730b429eb1e3a6184fa75b724402aac65f3
kane49 <kanedanachi7@gmail.com>
2015-07-13 20:10:47 +0900

    TimeManagar.cs修正

--- Assets/Member/Kaneda/Scripts/TimeManagar.cs
+++ Assets/Member/Kaneda/Scripts/TimeManagar.cs
@@ -7,24 +7,54 @@ public class TimeManagar : MonoBehaviour {
 	System.DateTime startTime;
 	//スコア表示するテキストをインスペクター上からアタッチ
 	public Text TimeText;
+	private float Timer = 0f;
+	private System.TimeSpan deltaTime;
+	private System.DateTime deltaTime2;
+
+	private string datetimeString;
 	
 	void Start () {
 		startTime = System.DateTime.Now;
-		
+
+		// 時刻の保存
+		//System.DateTime now = System.DateTime.Now;
+		//PlayerPrefs.SetString ("key", now.ToBinary().ToString() );
+		// 時刻の読み出し
+//		datetimeString = PlayerPrefs.GetString ("FastTimekey");
+//		System.DateTime fastTime = System.DateTime.FromBinary (System.Convert.ToInt64 (datetimeString));
 	}
 	
 	
 	void Update () {
 
-		System.DateTime now = System.DateTime.Now;
-		System.TimeSpan deltaTime = now - this.startTime;
+//		if (Goal.isGoal == false) {
+//			System.DateTime now = System.DateTime.Now;
+//			deltaTime = now - this.startTime;
+//			//deltaTime2 = now - this.startTime;
+//			//deltaTime2 = (System.TimeSpan)deltaTime;
+//
+//			TimeText.text = deltaTime.Minutes.ToString ("D2") + ":" 
+//				+ deltaTime.Seconds.ToString ("D2") + ":"
+//				+ deltaTime.Milliseconds.ToString ("D3");
+//		} else 
+//		{
+//			//ゴールした時間
+//			System.DateTime now = System.DateTime.Now;
+//			System.DateTime goalNow = now;
+//
+//
+//			//if()
+//
+//			PlayerPrefs.SetString ("key", deltaTime2.ToBinary().ToString() );//deltaTime
+//		}
 
-		TimeText.text = deltaTime.Minutes.ToString("D2") + ":" 
-			+ deltaTime.Seconds.ToString("D2")+ ":"
-			+ deltaTime.Milliseconds.ToString("D3");
+		Timer += Time.deltaTime;
 
-//		TimeText.text =  ((int)(Timer/60)).ToString("00")+":" 
-//			+ (((int)Timer)%60).ToString("00") + ":"
-//				  ;
+		TimeText.text =  ((int)(Timer/60)).ToString("00")
+			+":" 
+			+ (((int)Timer)%60).ToString("00") 
+			+ ":"
+				+(Mathf.Floor(Timer*1000)-(((int)Timer)*1000)).ToString("0,0,0");
+				  ;
 	}
 }
